b40063e88dae4106b950f542da54d18f489c9e55
nikhilraj286 <nikhilraj286@gmail.com>
2021-05-09 01:25:37 -0700

    get locations

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const app = require("../app");
 const router = express.Router();
 
 const auth = require("./userauth");
+const location = require("./location");
 
 router.use("/auth", auth);
+router.use("/location", location);
 
 module.exports = router;
